7259a2cd4e1aa9a5dbccb1e831a1f839482ffc15
Kevin W. Rudd <ke6fzi@gmail.com>
2024-07-10 13:00:02 -0400

    yet another attempt to get push to validate

--- test/tb.v
+++ test/tb.v
@@ -14,17 +14,17 @@ module tb ();
   end
 
   // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
+  reg           clk;
+  reg           rst_n;
+  reg           ena;
+  reg     [7:0] ui_in;
+  reg     [7:0] uio_in;
+  wire    [7:0] uo_out;
+  wire    [7:0] uio_out;
+  wire    [7:0] uio_oe;
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_overeducated_jumpstart    user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
